a9397347285460dc3f69327fc40b944bd39c5944
Greg Eisenhauer <eisen@cc.gatech.edu>
2002-02-21 19:46:34 +0000

    Update cercs_env to include high-res timing calls.  Probably will rearrange this at some point (and/or rename the project to something else).
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/cercs_env/trunk@6 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- cercs_env.h
+++ cercs_env.h
@@ -3,4 +3,44 @@
 
 extern char *cercs_getenv(char *name);
 extern int cercs_putenv(char *name_value);
+
+typedef struct chr_time {
+    double d1;
+    double d2;
+    double d3;
+} chr_time;
+
+extern void
+chr_get_time( chr_time *time);
+
+extern void
+chr_timer_diff( chr_time *diff_time, chr_time *src1, chr_time *src2);
+
+extern void
+chr_timer_zero( chr_time *time);
+
+extern void
+chr_timer_eq_zero( chr_time *time);
+
+extern void
+chr_timer_sum( chr_time *sum_time, chr_time *src1, chr_time *src2);
+
+extern void
+chr_timer_start (chr_time *timer);
+
+extern void
+chr_timer_stop (chr_time *timer);
+
+extern double
+chr_time_to_nanosecs (chr_time *time);
+
+extern double
+chr_time_to_microsecs (chr_time *time);
+
+extern double
+chr_time_to_millisecs (chr_time *time);
+
+extern double
+chr_time_to_secs (chr_time *time);
+
 #endif
