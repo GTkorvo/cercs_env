38be7bff7eaf935ede29849f6d49657ba44a4fdb
Greg Eisenhauer <eisen@cc.gatech.edu>
2012-02-07 17:05:40 +0000

    config.h prototype
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/cercs_env/trunk@10728 8ba6a8b9-845b-0410-94f0-974be2f5df47

---
+++ config.h.cmake
@@ -0,0 +1,11 @@
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H
+
+/* Location of installed config file */
+#cmakedefine INSTALL_DIR "@INSTALL_DIR@"
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS
+
